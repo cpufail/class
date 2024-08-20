20fbb57a7087fb003e5d6bed62339f2d5377048f
yasser <119708530+cpufail@users.noreply.github.com>
2024-08-19 22:54:10 -0300

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,24 @@
-lista = [2,6,1,8,9]
+def bubblesort(lista):
+    for i in range(len(lista)):
+        for j in range(0,(len(lista)-1)-i):
+            if (lista[j]>lista[j+1]):
+                aux=lista[j]
+                lista[j]=lista[j+1]
+                lista[j+1]= aux
 
-for i in range(len(lista)):
-    for j in range(0,(len(lista)-1)-i):
-        if (lista[j]>lista[j+1]):
-            aux=lista[j]
-            lista[j]=lista[j+1]
-            lista[j+1]= aux
-
+lista = [2,7,1,5,9]
+bubblesort(lista)
 print(lista)
+lista = [6,5,4,3,2]
+bubblesort(lista)
+print(lista)
+
+lista=[1,2,3,4]
+bubblesort(lista)
+print(lista)
+
+lista=[3,2,2,2,2,3,3]
+bubblesort(lista)
+print(lista)
+
 
